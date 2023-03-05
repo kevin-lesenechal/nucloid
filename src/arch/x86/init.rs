fffae1c3a9909b3f916d76bcf352d535800ce5c6
Kévin Lesénéchal <kevin.lesenechal@gmail.com>
2023-03-05 18:30:16 +0100

    Update copyright notice

--- src/arch/x86/init.rs
+++ src/arch/x86/init.rs
@@ -1,5 +1,5 @@
 /******************************************************************************
- * Copyright © 2021 Kévin Lesénéchal <kevin.lesenechal@gmail.com>             *
+ * Copyright © 2021-2023 Kévin Lesénéchal <kevin.lesenechal@gmail.com>        *
  * This file is part of the Nucloid operating system.                         *
  *                                                                            *
  * Nucloid is free software; you can redistribute it and/or modify it under   *
--- src/task/cpu_local.rs
+++ src/task/cpu_local.rs
@@ -1,3 +1,13 @@
+/******************************************************************************
+ * Copyright © 2021-2023 Kévin Lesénéchal <kevin.lesenechal@gmail.com>        *
+ * This file is part of the Nucloid operating system.                         *
+ *                                                                            *
+ * Nucloid is free software; you can redistribute it and/or modify it under   *
+ * the terms of the GNU General Public License as published by the Free       *
+ * Software Foundation; either version 2 of the License, or (at your option)  *
+ * any later version. See LICENSE file for more information.                  *
+ ******************************************************************************/
+
 use core::sync::atomic::Ordering;
 use crate::task::cpu::{CpuIndex, MAX_CPUS, NR_CPUS};
 
