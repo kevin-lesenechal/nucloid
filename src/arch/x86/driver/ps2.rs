982d807a78ab7a4d870cda6f8e889919f6305d47
Kévin Lesénéchal <kevin.lesenechal@gmail.com>
2023-04-02 16:56:04 +0200

    Add some copyright notices

--- src/arch/x86/driver/ps2.rs
+++ src/arch/x86/driver/ps2.rs
@@ -1,3 +1,13 @@
+/******************************************************************************
+ * Copyright © 2021-2023 Kévin Lesénéchal <kevin.lesenechal@gmail.com>        *
+ * This file is part of the Nucloid operating system.                         *
+ *                                                                            *
+ * Nucloid is free software; you can redistribute it and/or modify it under   *
+ * the terms of the GNU General Public License as published by the Free       *
+ * Software Foundation; either version 2 of the License, or (at your option)  *
+ * any later version. See LICENSE file for more information.                  *
+ ******************************************************************************/
+
 use x86::io::{inb, outb};
 
 use crate::arch::sync::{pop_critical_region, push_critical_region};
